2691c04d44aa5f6751158a32abc86a3acff1e27d
Nikhil Woodruff <nikhil@policyengine.org>
2022-12-10 18:47:09 +0000

    Add check

--- gcp/export.py
+++ gcp/export.py
@@ -2,8 +2,13 @@ import os
 from pathlib import Path
 
 GAE = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
-with open(GAE, "r") as f:
-    GAE = f.read()
+# If it's a filepath, read the file. Otherwise, it'll be JSON
+try:
+    Path(GAE).resolve(strict=True)
+    with open(GAE, "r") as f:
+        GAE = f.read()
+except FileNotFoundError:
+    pass
 DB_PD = os.environ["POLICYENGINE_DB_PASSWORD"]
 
 # Export GAE to to .gac.json and DB_PD to .dbpw in the current directory
