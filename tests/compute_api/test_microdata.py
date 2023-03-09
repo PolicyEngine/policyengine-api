eca4bdcd2d3a7e4a1cbdf877699cbf1afd7cea26
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-03-09 00:50:20 +0000

    Update PolicyEngine API

--- tests/compute_api/test_microdata.py
+++ tests/compute_api/test_microdata.py
@@ -7,6 +7,7 @@ def test_uk_microsim():
 
 def test_us_microsim():
     from policyengine_us import Microsimulation
+    from policyengine_core.simulations import MemoryConfig
     simulation = Microsimulation()
     simulation.memory_config = MemoryConfig(0.4)
     simulation.calculate("household_net_income")
