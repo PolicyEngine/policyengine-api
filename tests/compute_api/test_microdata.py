109f69857b72adf3cedc986eed1d88190cd536c6
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-03-09 00:09:34 +0000

    Update PolicyEngine API

--- tests/compute_api/test_microdata.py
+++ tests/compute_api/test_microdata.py
@@ -6,10 +6,10 @@ def test_uk_microsim():
 
 
 def test_us_microsim():
-    print(f"About to import")
+    print(f"About to import", flush=True)
     from policyengine_us import Microsimulation
-    print("Imported. Now instantiating microsim")
+    print("Imported. Now instantiating microsim", flush=True)
     simulation = Microsimulation()
-    print(f"Now running the tax-benefit system")
+    print(f"Now running the tax-benefit system", flush=True)
     simulation.calculate("household_net_income")
-    print(f"Done")
+    print(f"Done", flush=True)
