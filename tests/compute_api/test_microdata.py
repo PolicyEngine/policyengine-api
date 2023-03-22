f92b7a57abffe900dc4b21fa856f016b558f05ca
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-03-22 01:26:04 +0000

    Update PolicyEngine API

--- tests/compute_api/test_microdata.py
+++ tests/compute_api/test_microdata.py
@@ -9,10 +9,7 @@ from policyengine_uk import Microsimulation as UKMicrosimulation
 from policyengine_us import Microsimulation as USMicrosimulation
 import pytest
 
-PACKAGES_TO_TEST = [
-    UKMicrosimulation,
-    USMicrosimulation,
-]
+PACKAGES_TO_TEST = []
 
 
 @pytest.mark.parametrize("simulation_type", PACKAGES_TO_TEST, ids=["UK", "US"])
