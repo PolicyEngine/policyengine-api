e2ce37eaabeee577cac9ba575b5c698b2871ddd8
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-03-25 12:40:03 +0000

    Bump Core

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "click>=8",
         "flask>=1",
         "flask-cors>=3",
-        "PolicyEngine-Core>=1.12.1,<2",
+        "PolicyEngine-Core>=2,<3",
         "policyengine_uk==0.44.0",
         "policyengine_us==0.254.1",
         "policyengine_canada==0.42.3",
