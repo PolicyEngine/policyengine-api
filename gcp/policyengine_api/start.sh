ef7d07a8ea0a0fc8a9d964f4dac5bae62e6a99fb
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-04-07 14:12:40 +0100

    Update PolicyEngine API

--- gcp/policyengine_api/start.sh
+++ gcp/policyengine_api/start.sh
@@ -3,4 +3,4 @@ gunicorn -b :$PORT policyengine_api.api --timeout 300 --workers 1 &
 # Start the redis server
 redis-server &
 # Start the worker
-python3 policyengine_api/worker.py
+python3.9 policyengine_api/worker.py
