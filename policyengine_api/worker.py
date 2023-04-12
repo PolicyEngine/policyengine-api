0ea76118790176bba69b7f37c6b0e36b28819b12
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-04-12 10:01:54 +0100

    Optimise worker imports

--- policyengine_api/worker.py
+++ policyengine_api/worker.py
@@ -6,7 +6,9 @@ import policyengine_uk
 import policyengine_us
 import policyengine_canada
 import policyengine_ng
-from policyengine_api.endpoints.economy.reform_impact import set_reform_impact_data
+from policyengine_api.endpoints.economy.reform_impact import (
+    set_reform_impact_data,
+)
 
 # Provide the worker with the list of queues (str) to listen to.
 w = Worker(["default"], connection=Redis())
