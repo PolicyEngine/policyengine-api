2d5a90b326d9dee9d70dc2919c86815d21f89939
Sebastian Garcia Helguera <sebastiangarciahelguera@gmail.com>
2023-06-29 18:49:29 -0600

    Reformat code according to ms-python.black-formatter (I was using a different setting).

--- policyengine_api/api.py
+++ policyengine_api/api.py
@@ -32,13 +32,15 @@ from .endpoints import (
 
 app = application = flask.Flask(__name__)
 
-app.config.from_mapping({
-    "CACHE_TYPE": "RedisCache",
-    "CACHE_KEY_PREFIX": "policyengine",
-    "CACHE_REDIS_HOST": "127.0.0.1",
-    "CACHE_REDIS_PORT": 6379,
-    "CACHE_DEFAULT_TIMEOUT": 300
-    })
+app.config.from_mapping(
+    {
+        "CACHE_TYPE": "RedisCache",
+        "CACHE_KEY_PREFIX": "policyengine",
+        "CACHE_REDIS_HOST": "127.0.0.1",
+        "CACHE_REDIS_PORT": 6379,
+        "CACHE_DEFAULT_TIMEOUT": 300,
+    }
+)
 cache = Cache(app)
 
 CORS(app)
@@ -47,9 +49,7 @@ app.route("/", methods=["GET"])(get_home)
 
 app.route("/<country_id>/metadata", methods=["GET"])(get_metadata)
 
-app.route("/<country_id>/household/<household_id>", methods=["GET"])(
-    get_household
-)
+app.route("/<country_id>/household/<household_id>", methods=["GET"])(get_household)
 
 app.route("/<country_id>/household", methods=["POST"])(post_household)
 
@@ -74,24 +74,20 @@ app.route(
 )(cache.cached(make_cache_key=make_cache_key)(get_economic_impact))
 
 app.route("/<country_id>/analysis", methods=["POST"])(
-    app.route("/<country_id>/analysis/<prompt_id>", methods=["GET"])(
-        get_analysis
-    )
+    app.route("/<country_id>/analysis/<prompt_id>", methods=["GET"])(get_analysis)
 )
 
 app.route("/<country_id>/search", methods=["GET"])(get_search)
 
+
 @app.route("/liveness_check", methods=["GET"])
 def liveness_check():
-    return flask.Response(
-        "OK", status=200, headers={"Content-Type": "text/plain"}
-    )
+    return flask.Response("OK", status=200, headers={"Content-Type": "text/plain"})
+
 
 @app.route("/readiness_check", methods=["GET"])
 def readiness_check():
-    return flask.Response(
-        "OK", status=200, headers={"Content-Type": "text/plain"}
-    )
+    return flask.Response("OK", status=200, headers={"Content-Type": "text/plain"})
 
 
 # Add OpenAPI spec (__file__.parent / openapi_spec.yaml)
