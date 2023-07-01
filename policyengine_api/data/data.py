957cf4d40ea18ff203df40f5d494b9fdf1cf1730
Sukirna Roy <anrikus@outlook.com>
2023-07-01 00:17:16 -0700

    Diable changelog partially and point to correct GCP project

--- policyengine_api/data/data.py
+++ policyengine_api/data/data.py
@@ -38,7 +38,7 @@ class PolicyEngineDatabase:
 
     def _create_pool(self):
         instance_connection_name = (
-            "policyengine-api:us-central1:policyengine-api-data"
+            "policyengine-infrastructure:us-central1:policyengine-api-data"
         )
         self.connector = Connector()
         db_user = "policyengine"
