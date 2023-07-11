5e6ccba7f9da50d9c1d71737274c40e84420b6ac
Sebastian Garcia Helguera <sebastiangarciahelguera@gmail.com>
2023-07-11 12:42:52 -0700

    - Change test dir from tests/api to tests to run other tests on PR.  - added a test for /economy  - updated tests for /calculate

--- tests/python/test_calculate_us_1.py
+++ tests/python/test_calculate_us_1.py
@@ -1,6 +1,8 @@
+"""
 # Test: run a few calls to /calculate, running them with --durations=0 should
 # show that chaching is working (the ones suffixed by _repeat should be hits
 # and run much faster than their equivalent without the _repeat suffix).
+"""
 import pytest
 from policyengine_api.api import app
 
@@ -16,7 +18,7 @@ def client():
 def test_calculate_us_1(rest_client):
     """This should be a cache miss as no other requests have been made yet."""
     response = rest_client.post(
-        "http://localhost:5000/us/calculate",
+        "/us/calculate",
         headers={"Content-Type": "application/json"},
         data=open(
             "./tests/python/data/calculate_us_1_data.json",
@@ -24,14 +26,13 @@ def test_calculate_us_1(rest_client):
             encoding="utf-8",
         ),
     )
-    assert response.status_code == 200
-    print(response.text)
+    assert response.status_code == 200, response.text
 
 
 def test_calculate_us_2(rest_client):
     """This should be a miss as the data is different to test_calculate_us_1"""
     response = rest_client.post(
-        "http://localhost:5000/us/calculate",
+        "/us/calculate",
         headers={"Content-Type": "application/json"},
         data=open(
             "./tests/python/data/calculate_us_2_data.json",
@@ -39,14 +40,13 @@ def test_calculate_us_2(rest_client):
             encoding="utf-8",
         ),
     )
-    assert response.status_code == 200
-    print(response.text)
+    assert response.status_code == 200, response.text
 
 
 def test_calculate_us_1_repeat_1(rest_client):
     """This should be a hit as the data is the same as test_calculate_us_1"""
     response = rest_client.post(
-        "http://localhost:5000/us/calculate",
+        "/us/calculate",
         headers={"Content-Type": "application/json"},
         data=open(
             "./tests/python/data/calculate_us_1_data.json",
@@ -54,8 +54,7 @@ def test_calculate_us_1_repeat_1(rest_client):
             encoding="utf-8",
         ),
     )
-    assert response.status_code == 200
-    print(response.text)
+    assert response.status_code == 200, response.text
 
 
 def test_calculate_us_2_repeat_1(rest_client):
@@ -63,7 +62,7 @@ def test_calculate_us_2_repeat_1(rest_client):
     test_calculate_us_2
     """
     response = rest_client.post(
-        "http://localhost:5000/us/calculate",
+        "/us/calculate",
         headers={"Content-Type": "application/json"},
         data=open(
             "./tests/python/data/calculate_us_2_data.json",
@@ -71,5 +70,4 @@ def test_calculate_us_2_repeat_1(rest_client):
             encoding="utf-8",
         ),
     )
-    assert response.status_code == 200
-    print(response.text)
+    assert response.status_code == 200, response.text
