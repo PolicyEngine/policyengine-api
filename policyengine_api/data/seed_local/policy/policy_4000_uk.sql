c7352fcaa4b1e2a49428ec2432312a545b1af693
Anthony Volk <anth.volk@gmail.com>
2023-07-11 20:44:44 -0600

    feat: Added seed scripts for policy table

---
+++ policyengine_api/data/seed_local/policy/policy_4000_uk.sql
@@ -0,0 +1,82 @@
+INSERT INTO policy (id, country_id, label, api_version, policy_json, policy_hash)
+VALUES (
+	4000,
+	"uk",
+	NULL,
+	"0.8.3",
+	{
+        "gov.contrib.abolish_council_tax": {
+            "2023-01-01.2028-12-31": true
+        },
+        "gov.contrib.ubi_center.land_value_tax.corporate_rate": {
+            "2023-01-01.2028-12-31": 0.02
+        },
+        "gov.contrib.ubi_center.land_value_tax.household_rate": {
+            "2023-01-01.2028-12-31": 0.01
+        },
+        "gov.dcms.bbc.tv_licence.colour": {
+            "2023-01-01.2028-12-31": "0"
+        },
+        "gov.dwp.carers_allowance.rate": {
+            "2023-01-01.2028-12-31": "100"
+        },
+        "gov.dwp.disability_premia.disability_couple": {
+            "2023-01-01.2028-12-31": "100"
+        },
+        "gov.dwp.disability_premia.disability_single": {
+            "2023-01-01.2028-12-31": "75"
+        },
+        "gov.dwp.disability_premia.enhanced_couple": {
+            "2023-01-01.2028-12-31": "50"
+        },
+        "gov.dwp.disability_premia.enhanced_single": {
+            "2023-01-01.2028-12-31": "30"
+        },
+        "gov.dwp.disability_premia.severe_couple": {
+            "2023-01-01.2028-12-31": "200"
+        },
+        "gov.dwp.disability_premia.severe_single": {
+            "2023-01-01.2028-12-31": "100"
+        },
+        "gov.dwp.dla.mobility.higher": {
+            "2023-01-01.2028-12-31": "100"
+        },
+        "gov.dwp.dla.mobility.lower": {
+            "2023-01-01.2028-12-31": "50"
+        },
+        "gov.dwp.dla.self_care.higher": {
+            "2023-01-01.2028-12-31": "125"
+        },
+        "gov.dwp.dla.self_care.lower": {
+            "2023-01-01.2028-12-31": "50"
+        },
+        "gov.dwp.dla.self_care.middle": {
+            "2023-01-01.2028-12-31": "80"
+        },
+        "gov.dwp.pip.daily_living.enhanced": {
+            "2023-01-01.2028-12-31": "150"
+        },
+        "gov.dwp.pip.daily_living.standard": {
+            "2023-01-01.2028-12-31": "100"
+        },
+        "gov.hmrc.fuel_duty.petrol_and_diesel": {
+            "2023-01-01.2028-12-31": "0"
+        },
+        "gov.hmrc.income_tax.allowances.personal_allowance.amount": {
+            "2023-01-01.2028-12-31": "15000"
+        },
+        "gov.hmrc.income_tax.rates.uk[0].rate": {
+            "2023-01-01.2028-12-31": 0.18
+        },
+        "gov.hmrc.national_insurance.class_1.rates.employee.additional": {
+            "2023-01-01.2028-12-31": 0.04
+        },
+        "gov.hmrc.national_insurance.class_1.rates.employee.main": {
+            "2023-01-01.2028-12-31": 0.13
+        },
+        "gov.hmrc.national_insurance.class_1.rates.employer": {
+            "2023-01-01.2028-12-31": "0.2"
+        }
+    },
+	"znzaNCNYTusu1fs6gpzH05ov9ASoIp2nBeRclGQ01tc="
+);
