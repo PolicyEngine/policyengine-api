b0213da39abcb64efc752ad33f8f02e6dbbec797
Sebastian Garcia Helguera <sebastiangarciahelguera@gmail.com>
2023-08-03 14:13:25 -0700

    Replace "python" with sys.executable to use the same python being run

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -2,7 +2,7 @@
 import time
 from contextlib import contextmanager
 from subprocess import Popen, TimeoutExpired
-import os
+import sys
 import redis
 import pytest
 from policyengine_api.api import app
@@ -11,7 +11,7 @@ from policyengine_api.api import app
 @contextmanager
 def running(process_arguments, seconds_to_wait_after_launch=0):
     """run a process and kill it after"""
-    process = Popen(process_arguments, shell=True, env=os.environ)
+    process = Popen(process_arguments)
     time.sleep(seconds_to_wait_after_launch)
     try:
         yield process
@@ -30,6 +30,6 @@ def client():
     with running(["redis-server"], 3):
         redis_client = redis.Redis()
         redis_client.ping()
-        with running(["python", "policyengine_api/worker.py"], 3):
+        with running([sys.executable, "policyengine_api/worker.py"], 3):
             with app.test_client() as test_client:
                 yield test_client
