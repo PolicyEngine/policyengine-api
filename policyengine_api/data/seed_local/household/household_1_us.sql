07d809cd4eb3b6bafaec6211f4412a1a2c0817ec
Anthony Volk <anth.volk@gmail.com>
2023-08-03 18:42:51 -0600

    feat: Added tests for US households that represent broad cross-section of db

---
+++ policyengine_api/data/seed_local/household/household_1_us.sql
@@ -0,0 +1,42 @@
+INSERT INTO household (id, country_id, label, api_version, household_json, household_hash)
+VALUES (
+	501,
+	"us",
+	"Sample dataset - duplicate of US household #4200 in live database",
+	"0.8.9",
+	{
+        "households": {
+            "household": {
+                "household_benefits": {
+                    "2023": null
+                },
+                "household_net_income": {
+                    "2023": null
+                },
+                "household_tax": {
+                    "2023": null
+                },
+                "members": [
+                    "parent",
+                    "child"
+                ]
+            }
+        },
+        "people": {
+            "child": {
+                "age": {
+                    "2023": 10
+                }
+            },
+            "parent": {
+                "age": {
+                    "2023": 35
+                },
+                "employment_income": {
+                    "2023": 30000
+                }
+            }
+        }
+    },
+	"UdeoPHrOn8g6qnMvyfBUjjGVl1ClHBpNGnvIC+N5oIQ="
+);
