23ad52e5b6eb6c32a38bd75e90ae7607a386cae1
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-11-22 16:07:18 +0000

    Bump Core to 2.11 (#903)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "gunicorn",
         "markupsafe==2.0.1",
         "openai",
-        "PolicyEngine-Core>=2.1,<3",
+        "PolicyEngine-Core>=2.11,<3",
         "policyengine_canada==0.87.0",
         "policyengine-ng==0.5.1",
         "policyengine-il==0.1.0",
