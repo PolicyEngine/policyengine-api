9761098503f330679930e33820acfe35eacab760
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-11-28 12:02:15 +0000

    Bump Core

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "gunicorn",
         "markupsafe==2.0.1",
         "openai",
-        "PolicyEngine-Core==2.9.0",
+        "PolicyEngine-Core>=2.10,<3",
         "policyengine_canada==0.87.0",
         "policyengine-ng==0.5.1",
         "policyengine-il==0.1.0",
