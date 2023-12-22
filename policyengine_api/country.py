82b616ff63af778f23e7739e9b9e117aa4d8a854
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-12-22 16:35:07 +0000

    Add 2024

--- policyengine_api/country.py
+++ policyengine_api/country.py
@@ -137,6 +137,7 @@ class PolicyEngineCountry:
             ]
             time_period = [
                 dict(name=2023, label="2023"),
+                dict(name=2024, label="2024"),
                 dict(name=2022, label="2022"),
                 dict(name=2021, label="2021"),
             ]
