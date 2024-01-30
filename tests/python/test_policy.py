23e2b61fe32dbc214fb80c0b933fd4b6c4cc47e7
Anthony Volk <anth.volk@gmail.com>
2024-01-30 19:47:10 +0200

    test: Add tests for changes to set_policy

---
+++ tests/python/test_policy.py
@@ -0,0 +1,42 @@
+import pytest
+import json
+from policyengine_api.data import database
+
+class TestPolicy:
+  # Define the policy to test against
+  country_id = "us"
+  policy_json = {
+    "sample_parameter": "maxwell"
+  },
+  label = "cruft"
+  test_policy = {
+    "data": policy_json,
+    "label": label
+  }
+
+  """
+  Test creating a policy, then ensure that duplicating
+  that policy generates the correct response within the
+  app; this requires sequential processing, hence the 
+  need for a separate Python-based test
+  """
+  def test_create_unique_policy(self, rest_client):
+
+    res = rest_client.post("/us/policy", json=self.test_policy)
+    return_object = json.loads(res.text)
+  
+    assert return_object["status"] == "ok"
+    assert res.status_code == 201
+  
+  def test_create_nonunique_policy(self, rest_client):
+  
+    res = rest_client.post("/us/policy", json=self.test_policy)
+    return_object = json.loads(res.text)
+  
+    assert return_object["status"] == "ok"
+    assert res.status_code == 200
+
+    database.query(
+      f"DELETE FROM policy WHERE policy_json IS ? AND label IS ? AND country_id IS ?",
+      (json.dumps(self.policy_json), self.label, self.country_id)
+    )
