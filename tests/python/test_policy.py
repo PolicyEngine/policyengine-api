dd8e9adc3fa844ae4308d5b0c8c181b4a3dcf27d
Anthony V <anth.volk@gmail.com>
2024-02-14 14:37:31 +0200

    Improve `set_policy` endpoint (#1144)
    * fix: Change = to IS in WHERE clauses
    
    * test: Add tests for changes to query method of database connector
    
    * fix: Alter functional structure of set_policy
    
    * test: Add tests for changes to set_policy
    
    * chore: Lint and changelog
    
    * fix: Remove IS selector from SQL statements
    
    * fix: Conditionally generate SQL statement within set_policy
    
    * fix: Repair tests, comment that changes should be superseded with ORM
    
    * test: Ensure test_economy_1.py always has new version of policy
    
    * chore: Remove dead log
    
    * fix: Correct test_economy_1.py to pre-delete any previous tests
    
    * test: Fix test_create_unique_policy to pre-delete preformed tests
    
    * test: Fix create empty US policy test
    
    * chore: Lint
    
    * test: Fix test_economy_1.py
    
    * test: Fix test_policy.py

---
+++ tests/python/test_policy.py
@@ -0,0 +1,44 @@
+import pytest
+import json
+from policyengine_api.data import database
+
+
+class TestPolicy:
+    # Define the policy to test against
+    country_id = "us"
+    policy_json = ({"sample_parameter": "maxwell"},)
+    label = "test_label"
+    test_policy = {"data": policy_json, "label": label}
+
+    """
+  Test creating a policy, then ensure that duplicating
+  that policy generates the correct response within the
+  app; this requires sequential processing, hence the 
+  need for a separate Python-based test
+  """
+
+    def test_create_unique_policy(self, rest_client):
+
+        database.query(
+            f"DELETE FROM policy WHERE policy_json = ? AND label = ? AND country_id = ?",
+            (json.dumps(self.policy_json), self.label, self.country_id),
+        )
+
+        res = rest_client.post("/us/policy", json=self.test_policy)
+        return_object = json.loads(res.text)
+
+        assert return_object["status"] == "ok"
+        assert res.status_code == 201
+
+    def test_create_nonunique_policy(self, rest_client):
+
+        res = rest_client.post("/us/policy", json=self.test_policy)
+        return_object = json.loads(res.text)
+
+        assert return_object["status"] == "ok"
+        assert res.status_code == 200
+
+        database.query(
+            f"DELETE FROM policy WHERE policy_json = ? AND label = ? AND country_id = ?",
+            (json.dumps(self.policy_json), self.label, self.country_id),
+        )
