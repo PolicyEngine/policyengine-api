1422983a93683b552662792933cdd33e77873154
Abhishek Singh <31144719+abhcs@users.noreply.github.com>
2024-04-01 11:07:27 -0500

    fix: propagate units to descendants for ParameterScale objects

---
+++ tests/python/test_units.py
@@ -0,0 +1,23 @@
+from policyengine_api.endpoints.metadata import get_metadata
+
+
+def test_units():
+    m = get_metadata("us")
+    assert (
+        m["result"]["parameters"][
+            "gov.states.md.tax.income.rates.head[0].rate"
+        ]["unit"]
+        == "/1"
+    )
+    assert (
+        m["result"]["parameters"][
+            "gov.states.md.tax.income.rates.head[0].threshold"
+        ]["unit"]
+        == "currency-USD"
+    )
+    assert (
+        m["result"]["parameters"][
+            "gov.irs.credits.eitc.phase_out.start[0].amount"
+        ]["unit"]
+        == "currency-USD"
+    )
