b83bf394820dc7159c2f3ae520f9669ce3ff2f7d
Anthony Volk <anth.volk@gmail.com>
2024-04-11 16:16:11 +0200

    fix: Test against local_database to avoid fact that remote database is not altered while deployed

--- tests/python/test_user_policy.py
+++ tests/python/test_user_policy.py
@@ -1,9 +1,8 @@
 import json
-from policyengine_api.data import database
+from policyengine_api.data import local_database
 
 
 class TestUserPolicies:
-
     # Define the policy to test against
     country_id = "us"
     policy_id = 2
@@ -16,7 +15,7 @@ class TestUserPolicies:
   """
 
     def test_set_and_get_record(self, rest_client):
-        database.query(
+        local_database.query(
             f"DELETE FROM user_policies WHERE policy_id = ? AND user_id = ? AND label = ?",
             (self.policy_id, self.user_id, self.label),
         )
@@ -33,7 +32,7 @@ class TestUserPolicies:
         assert return_object["status"] == "ok"
         assert return_object["result"][0]["policy_id"] == self.policy_id
 
-        database.query(
+        local_database.query(
             f"DELETE FROM user_policies WHERE policy_id = ? AND user_id = ? AND label = ?",
             (self.policy_id, self.user_id, self.label),
         )
