3a8732ecaf8f898579b4ca39dcbf2cd428c4ec5e
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-06-11 09:09:50 +0100

    Bump PolicyEngine UK to 0.82.0 (#1567)
    Co-authored-by: PolicyEngine[bot] <hello@policyengine.org>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "policyengine_canada==0.95.0",
         "policyengine-ng==0.5.1",
         "policyengine-il==0.1.0",
-        "policyengine_uk==0.80.0",
+        "policyengine_uk==0.82.0",
         "policyengine_us==0.785.1",
         "pymysql",
         "redis",
