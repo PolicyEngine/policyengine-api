4896994a44880f3132d5af7fd524a72819af1062
leehengpan <lihengpanza@gmail.com>
2024-09-09 14:33:17 -0400

    Update country.py

--- policyengine_api/country.py
+++ policyengine_api/country.py
@@ -512,7 +512,7 @@ def get_all_variables(variable_name: str, system: TaxBenefitSystem, variables: l
     subtracts = variable.subtracts
 
     if adds is None and subtracts is None:
-        return variables
+        return []
 
     if adds is not None:
         for add in adds:
