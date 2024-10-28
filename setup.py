e3743482fbe18d55ed83d5020dc2450fd97bb98f
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-10-28 12:30:19 +0000

    Update PolicyEngine UK to 2.14.0 (#1921)
    * Bump PolicyEngine UK to 2.14.0
    
    * Move back to 3.10
    
    ---------
    
    Co-authored-by: PolicyEngine[bot] <hello@policyengine.org>

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "policyengine_canada==0.96.1",
         "policyengine-ng==0.5.1",
         "policyengine-il==0.1.0",
-        "policyengine_uk==2.13.2",
+        "policyengine_uk==2.14.0",
         "policyengine_us==1.133.0",
         "policyengine_core>=3.11.0",
         "pymysql",
