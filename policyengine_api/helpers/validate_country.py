66a3a940998b6210d6ac0ff8e0406284e9d1a505
ldorner <ld@Thes-MacBook-Pro.local>
2024-11-12 00:09:53 -0800

    Switch @validate_country to decorator

--- policyengine_api/helpers/validate_country.py
+++ policyengine_api/helpers/validate_country.py
@@ -1,22 +1,30 @@
+from functools import wraps
 from typing import Union
 from flask import Response
 import json
 from policyengine_api.constants import COUNTRIES
 
 
-def validate_country(country_id: str) -> Union[None, Response]:
+def validate_country(func):
     """Validate that a country ID is valid. If not, return a 404 response.
 
     Args:
         country_id (str): The country ID to validate.
 
     Returns:
-
+        Response(404) if country is not valid, else continues
     """
-    if country_id not in COUNTRIES:
-        body = dict(
-            status="error",
-            message=f"Country {country_id} not found. Available countries are: {', '.join(COUNTRIES)}",
-        )
-        return Response(json.dumps(body), status=404)
-    return None
+
+    @wraps(func)
+    def validate_country_wrapper(
+        country_id: str, *args, **kwargs
+    ) -> Union[None, Response]:
+        if country_id not in COUNTRIES:
+            body = dict(
+                status="error",
+                message=f"Country {country_id} not found. Available countries are: {', '.join(COUNTRIES.keys())}",
+            )
+            return Response(json.dumps(body), status=404)
+        return func(country_id, *args, **kwargs)
+
+    return validate_country_wrapper
---
+++ tests/python/test_country.py
@@ -0,0 +1,27 @@
+from flask import Response
+from policyengine_api.country import validate_country
+
+
+@validate_country
+def foo(country_id, other):
+    """
+    A simple dummy test method for validation testing. Must be defined outside of the class (or within
+    the test functions themselves) due to complications with the `self` parameter for class methods.
+    """
+    return "bar"
+
+
+class TestValidateCountry:
+    """
+    Test that the @validate_country decorator returns 404 if the country does not exist, otherwise
+    continues execution of the function.
+    """
+
+    def test_valid_country(self):
+        result = foo("us", "extra_arg")
+        assert result == "bar"
+
+    def test_invalid_country(self):
+        result = foo("baz", "extra_arg")
+        assert isinstance(result, Response)
+        assert result.status_code == 404
