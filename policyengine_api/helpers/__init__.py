9b513af25e27191c7d376d26529173333dd9cbc0
Anthony Volk <anth.volk@gmail.com>
2024-11-25 18:00:22 +0100

    chore: Move test fixtures, drop helpers folder in favor of utils

--- policyengine_api/helpers/__init__.py
+++
@@ -1,1 +0,0 @@
-from .get_current_law import get_current_law_policy_id
--- tests/python/test_simulation_analysis.py
+++ tests/python/test_simulation_analysis.py
@@ -9,6 +9,8 @@ from policyengine_api.routes.simulation_analysis_routes import (
     execute_simulation_analysis,
 )
 
+from tests.fixtures.simulation_analysis_fixtures import test_json, test_impact
+
 test_service = SimulationAnalysisService()
 
 
@@ -17,36 +19,6 @@ def app():
     app = Flask(__name__)
     app.config["TESTING"] = True
     return app
-
-
-test_impact = {
-    "budget": 1000,
-    "intra_decile": 0.1,
-    "decile": 0.2,
-    "poverty": {
-        "poverty": 0.3,
-        "deep_poverty": 0.4,
-    },
-    "poverty_by_gender": 0.5,
-    "poverty_by_race": {"poverty": 0.6},
-    "inequality": 0.7,
-}
-
-test_json = {
-    "currency": "USD",
-    "selected_version": "2023",
-    "time_period": "2023",
-    "impact": test_impact,
-    "policy_label": "Test Policy",
-    "policy": dict(policy_json="policy details"),
-    "region": "US",
-    "relevant_parameters": ["param1", "param2"],
-    "relevant_parameter_baseline_values": [
-        {"param1": 100},
-        {"param2": 200},
-    ],
-    "audience": "Normal",
-}
 
 
 def test_execute_simulation_analysis_existing_analysis(app, rest_client):
