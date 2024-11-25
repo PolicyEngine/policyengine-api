ccd45167290df1eb5c1601e4e152935da260819d
Anthony Volk <anth.volk@gmail.com>
2024-11-25 20:48:43 +0100

    feat: Create Python data files for storing test policies

---
+++ tests/data/test_policies_states.py
@@ -0,0 +1,12 @@
+
+or_rebate_measure_118 = {
+  "gov.contrib.ubi_center.basic_income.amount.person.flat": {
+    "2024-01-01.2100-12-31": 1160
+  },
+  "region": "or"
+}
+
+
+all_policies = [
+  or_rebate_measure_118
+]
