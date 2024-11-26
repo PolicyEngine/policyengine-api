07569b75ac4eb945574fa6efecfc09a3330fdcdf
M_A_R_S_O_X <marek.soltes22@gmial.com>
2024-11-26 13:48:36 +0100

    fixissue1944_2

--- policyengine_api/data/data.py
+++ policyengine_api/data/data.py
@@ -171,8 +171,7 @@ class PolicyEngineDatabase:
             )
 
 
-# Determine if app is in debug mode, and if so, do not attempt connection with remote db
-if os.environ.get("FLASK_DEBUG") == "1":
+if os.environ.get("FLASK_ENV") == "development":
     database = PolicyEngineDatabase(local=True, initialize=False)
 else:
     database = PolicyEngineDatabase(local=False, initialize=False)
