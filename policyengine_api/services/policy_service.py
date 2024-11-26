a55f7ec87bb0249628536401e547d3c442c30944
Anthony Volk <anth.volk@gmail.com>
2024-11-26 22:18:51 +0100

    chore: Migrate get_policy to new format

--- policyengine_api/services/policy_service.py
+++ policyengine_api/services/policy_service.py
@@ -8,6 +8,18 @@ class PolicyService:
     to the policy database table
     """
 
+    def get_policy(self, country_id: str, policy_id: int) -> dict | None:
+        try:
+            # If no policy found, this will return None
+            policy = database.query(
+                "SELECT * FROM policy WHERE country_id = ? AND id = ?",
+                (country_id, policy_id),
+            ).fetchone()
+            return policy
+        except Exception as e:
+            print(f"Error getting policy: {str(e)}")
+            raise e
+
     def get_policy_json(self, country_id, policy_id):
         try:
             policy_json = database.query(
