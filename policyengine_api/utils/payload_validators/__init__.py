506d2333ee415cacc8d2798b50ca77d68c63d858
Anthony Volk <anth.volk@gmail.com>
2024-11-30 06:30:06 +0100

    feat: Migrate update household endpoint

--- policyengine_api/utils/payload_validators/__init__.py
+++ policyengine_api/utils/payload_validators/__init__.py
@@ -1,4 +1,4 @@
 from .validate_sim_analysis_payload import validate_sim_analysis_payload
 from .validate_tracer_analysis_payload import validate_tracer_analysis_payload
 from .validate_country import validate_country
-from .validate_post_household_payload import validate_post_household_payload
+from .validate_household_payload import validate_household_payload
