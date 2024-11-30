30c969a847733aa74026d264e646651a9b2cca71
Anthony Volk <anth.volk@gmail.com>
2024-11-30 06:12:14 +0100

    feat: Migrate post_household to new structure

--- policyengine_api/routes/household_routes.py
+++ policyengine_api/routes/household_routes.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 import json
-from policyengine_api.utils.payload_validators import validate_country
+
+from policyengine_api.constants import COUNTRY_PACKAGE_VERSIONS
 from policyengine_api.services.household_service import HouseholdService
+from policyengine_api.utils import hash_object
+from policyengine_api.utils.payload_validators import (
+  validate_post_household_payload,
+  validate_country
+)
 
 
 household_bp = Blueprint("household", __name__)
@@ -60,6 +66,62 @@ def get_household(country_id: str, household_id: str) -> dict:
             status=500
         )
 
+@validate_country
+@household_bp.route("/<country_id>/household", methods=["POST"])
+def post_household(country_id: str) -> dict:
+    """
+    Set a household's input data.
+
+    Args:
+        country_id (str): The country ID.
+    """
+
+    # Validate payload
+    payload = request.json
+    is_payload_valid, message = validate_post_household_payload(payload)
+    if not is_payload_valid:
+        return Response(
+            status=400,
+            response=f"Unable to create new household; details: {message}",
+        )
+    
+    try:
+        # The household label appears to be unimplemented at this time,
+        # thus it should always be 'None'
+        label: str | None = payload.get("label")
+        household_json: dict = payload.get("data")
+        household_hash: str = hash_object(household_json)
+        api_version: str = COUNTRY_PACKAGE_VERSIONS.get(country_id)
+
+        household_id = household_service.create_household(
+            country_id, household_json, household_hash, label, api_version
+        )
+
+        return Response(
+            json.dumps(
+                {
+                    "status": "ok",
+                    "message": None,
+                    "result": {
+                        "household_id": household_id,
+                    }
+                }
+            ), 
+            status=201,
+            mimetype="application/json"
+        )
+
+    except Exception as e:
+        return Response(
+            json.dumps(
+                {
+                    "status": "error",
+                    "message": f"An error occurred while creating a new household. Details: {str(e)}",
+                }
+            ),
+            status=500,
+            mimetype="application/json"
+        )
 
 
 # app.route("/<country_id>/household/<household_id>", methods=["GET"])(
--- policyengine_api/services/household_service.py
+++ policyengine_api/services/household_service.py
@@ -31,3 +31,46 @@ class HouseholdService:
     except Exception as e:
        print(f"Error fetching household #{household_id}. Details: {str(e)}")
        raise e
+    
+  def create_household(
+      self,
+      country_id: str,
+      household_json: dict,
+      household_hash: int,
+      label: str | None,
+      api_version: str,
+  ) -> int:
+      """
+      Create a new household with the given data.
+
+      Args:
+          country_id (str): The country ID.
+          household_json (dict): The household data.
+          household_hash (int): The hash of the household data.
+          label (str): The label for the household.
+          api_version (str): The API version.
+      """
+
+      print("Creating new household")
+
+      try:
+          database.query(
+              f"INSERT INTO household (country_id, household_json, household_hash, label, api_version) VALUES (?, ?, ?, ?, ?)",
+              (
+                  country_id,
+                  json.dumps(household_json),
+                  household_hash,
+                  label,
+                  api_version,
+              ),
+          )
+
+          household_id = database.query(
+              f"SELECT id FROM household WHERE country_id = ? AND household_hash = ?",
+              (country_id, household_hash),
+          ).fetchone()["id"]
+
+          return household_id
+      except Exception as e:
+          print(f"Error creating household. Details: {str(e)}")
+          raise e
