54ad24a3a4357e8420dacbf93ecc0b5c4cb12c28
Anthony Volk <anth.volk@gmail.com>
2025-01-17 16:32:47 +0100

    fix: Use standardized error handling

--- policyengine_api/routes/policy_routes.py
+++ policyengine_api/routes/policy_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, Response, request
 import json
 
 from policyengine_api.services.policy_service import PolicyService
+from werkzeug.exceptions import NotFound, BadRequest
 from policyengine_api.utils.payload_validators import (
     validate_country,
     validate_set_policy_payload,
@@ -28,41 +29,20 @@ def get_policy(country_id: str, policy_id: int | str) -> Response:
         policy data in JSON format
     """
 
-    try:
-        # Specifically cast policy_id to an integer
-        policy_id = int(policy_id)
+    # Specifically cast policy_id to an integer
+    policy_id = int(policy_id)
 
-        policy: dict | None = policy_service.get_policy(country_id, policy_id)
+    policy: dict | None = policy_service.get_policy(country_id, policy_id)
 
-        if policy is None:
-            return Response(
-                json.dumps(
-                    {
-                        "status": "error",
-                        "message": f"Policy #{policy_id} not found",
-                    }
-                ),
-                status=404,
-            )
-        else:
-            return Response(
-                json.dumps(
-                    {"status": "ok", "message": None, "result": policy}
-                ),
-                status=200,
-            )
+    if policy is None:
+        raise NotFound(f"Policy #{policy_id} not found.")
 
-    except Exception as e:
-        return Response(
-            json.dumps(
-                {
-                    "status": "error",
-                    "message": f"An error occurred while fetching policy data: {str(e)}",
-                },
-                status=500,
-            )
-        )
-
+    return Response(
+        json.dumps(
+            {"status": "ok", "message": None, "result": policy}
+        ),
+        status=200,
+    )
 
 @policy_bp.route("/<country_id>/policy", methods=["POST"])
 @validate_country
@@ -80,40 +60,26 @@ def set_policy(country_id: str) -> Response:
 
     is_payload_valid, message = validate_set_policy_payload(payload)
     if not is_payload_valid:
-        return Response(
-            status=400, response=f"Invalid JSON data; details: {message}"
-        )
+        raise BadRequest(f"Invalid JSON data; details: {message}")
 
     label = payload.pop("label", None)
     policy_json = payload.pop("data", None)
 
-    try:
-        policy_id, message, is_existing_policy = policy_service.set_policy(
-            country_id,
-            label,
-            policy_json,
-        )
+    policy_id, message, is_existing_policy = policy_service.set_policy(
+        country_id,
+        label,
+        policy_json,
+    )
 
-        response_body = dict(
-            status="ok",
-            message=message,
-            result=dict(
-                policy_id=policy_id,
-            ),
-        )
+    response_body = dict(
+        status="ok",
+        message=message,
+        result=dict(
+            policy_id=policy_id,
+        ),
+    )
 
-        code = 200 if is_existing_policy else 201
-        return Response(
-            json.dumps(response_body), status=code, mimetype="application/json"
-        )
-
-    except Exception as e:
-        return Response(
-            json.dumps(
-                {
-                    "status": "error",
-                    "message": f"An error occurred while setting policy data: {str(e)}",
-                },
-                status=500,
-            )
-        )
+    code = 200 if is_existing_policy else 201
+    return Response(
+        json.dumps(response_body), status=code, mimetype="application/json"
+    )
