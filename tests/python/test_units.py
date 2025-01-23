8f6b6d4b10c2624a2d4ddb36ffb0491bb42c82d7
Herve R. Iradukunda <herve.irad@gmail.com>
2025-01-23 18:28:06 -0500

    Reformat

--- tests/python/test_units.py
+++ tests/python/test_units.py
@@ -6,20 +6,18 @@ metadata_service = MetadataService()
 def test_units():
     m = metadata_service.get_metadata("us")
     assert (
-        m["parameters"][
-            "gov.states.md.tax.income.rates.head[0].rate"
-        ]["unit"]
+        m["parameters"]["gov.states.md.tax.income.rates.head[0].rate"]["unit"]
         == "/1"
     )
     assert (
-        m["parameters"][
-            "gov.states.md.tax.income.rates.head[0].threshold"
-        ]["unit"]
+        m["parameters"]["gov.states.md.tax.income.rates.head[0].threshold"][
+            "unit"
+        ]
         == "currency-USD"
     )
     assert (
-        m["parameters"][
-            "gov.irs.credits.eitc.phase_out.start[0].amount"
-        ]["unit"]
+        m["parameters"]["gov.irs.credits.eitc.phase_out.start[0].amount"][
+            "unit"
+        ]
         == "currency-USD"
     )
