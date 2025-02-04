fea31b2c7dc1acd86acbaa8ce606f6ec3b34fda3
Anthony Volk <anth.volk@gmail.com>
2025-02-04 18:41:26 +0100

    test: Remove policy search test, add conftest

--- tests/python/test_policy.py
+++ tests/python/test_policy.py
@@ -49,55 +49,59 @@ class TestPolicyCreation:
         assert res.status_code == 400
 
 
-class TestPolicySearch:
-    country_id = "us"
-    policy_json = {"sample_input": {"2023-01-01.2024-12-31": True}}
-    label = "maxwell"
-    policy_hash = hash_object(policy_json)
-    api_version = COUNTRY_PACKAGE_VERSIONS.get(country_id)
+# The below test is very prone to race conditions due to emission against live db;
+# should be refactored to use mock/test db and reinstated
 
-    # Pre-seed database with duplicate policies
-    for i in range(2):
-        database.query(
-            f"INSERT INTO policy (country_id, label, policy_json, policy_hash, api_version) VALUES (?, ?, ?, ?, ?)",
-            (
-                country_id,
-                label,
-                json.dumps(policy_json),
-                policy_hash,
-                api_version,
-            ),
-        )
-
-    db_output = database.query(
-        f"SELECT * FROM policy WHERE label = ?",
-        (label,),
-    ).fetchall()
-
-    def test_search_all_policies(self, rest_client):
-        res = rest_client.get("/us/policies")
-        return_object = json.loads(res.text)
-
-        filtered_return = list(
-            filter(lambda x: x["label"] == self.label, return_object["result"])
-        )
-
-        assert return_object["status"] == "ok"
-        assert len(filtered_return) == len(self.db_output)
-
-    def test_search_unique_policies(self, rest_client):
-        res = rest_client.get("/us/policies?unique_only=true")
-        return_object = json.loads(res.text)
-
-        filtered_return = list(
-            filter(lambda x: x["label"] == self.label, return_object["result"])
-        )
-
-        assert return_object["status"] == "ok"
-        assert len(filtered_return) == 1
-
-        # Clean up duplicate policies created
-        database.query(
-            f"DELETE FROM policy WHERE policy_hash = ? AND label = ? AND country_id = ?",
-            (self.policy_hash, self.label, self.country_id),
-        )
+# class TestPolicySearch:
+#     country_id = "us"
+#     policy_json = {"sample_input": {"2023-01-01.2024-12-31": True}}
+#     label = "maxwell"
+#     policy_hash = hash_object(policy_json)
+#     api_version = COUNTRY_PACKAGE_VERSIONS.get(country_id)
+#
+#     # Pre-seed database with duplicate policies
+#     for i in range(2):
+#         database.query(
+#             f"INSERT INTO policy (country_id, label, policy_json, policy_hash, api_version) VALUES (?, ?, ?, ?, ?)",
+#             (
+#                 country_id,
+#                 label,
+#                 json.dumps(policy_json),
+#                 policy_hash,
+#                 api_version,
+#             ),
+#         )
+#
+#     db_output = database.query(
+#         f"SELECT * FROM policy WHERE label = ?",
+#         (label,),
+#     ).fetchall()
+#
+#     def test_search_all_policies(self, rest_client):
+#         res = rest_client.get("/us/policies")
+#         return_object = json.loads(res.text)
+#
+#         filtered_return = list(
+#             filter(lambda x: x["label"] == self.label, return_object["result"])
+#         )
+#
+#         assert return_object["status"] == "ok"
+#         assert len(filtered_return) == len(self.db_output)
+#
+#     def test_search_unique_policies(self, rest_client):
+#         res = rest_client.get("/us/policies?unique_only=true")
+#         return_object = json.loads(res.text)
+#
+#         filtered_return = list(
+#             filter(lambda x: x["label"] == self.label, return_object["result"])
+#         )
+#
+#         assert return_object["status"] == "ok"
+#         assert len(filtered_return) == 1
+#
+#         # Clean up duplicate policies created
+#         database.query(
+#             f"DELETE FROM policy WHERE policy_hash = ? AND label = ? AND country_id = ?",
+#             (self.policy_hash, self.label, self.country_id),
+#         )
+#
