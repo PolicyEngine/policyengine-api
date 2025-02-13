14c2913d76c582bc65be8873b26b033c91a0f565
Anthony Volk <anth.volk@gmail.com>
2025-02-13 22:36:31 +0100

    fix: Improve tests

--- tests/unit/ai_prompts/test_simulation_analysis_prompt.py
+++ tests/unit/ai_prompts/test_simulation_analysis_prompt.py
@@ -3,19 +3,57 @@ from policyengine_api.ai_prompts.simulation_analysis_prompt import (
     generate_simulation_analysis_prompt,
 )
 from tests.fixtures.simulation_analysis_prompt_fixtures import (
-    valid_input_data,
+    valid_input_us,
+    valid_input_uk,
     invalid_data_missing_input_field,
+    given_valid_data_and_region_is_enhanced_us,
+    given_valid_data_and_dataset_is_enhanced_cps,
 )
 
 
 class TestGenerateSimulationAnalysisPrompt:
 
-    def test_given_all_required_keys_present(self, snapshot):
+    def test_given_valid_us_input(self, snapshot):
 
         snapshot.snapshot_dir = "tests/snapshots"
 
-        prompt = generate_simulation_analysis_prompt(valid_input_data)
-        snapshot.assert_match(prompt, "simulation_analysis_prompt.txt")
+        prompt = generate_simulation_analysis_prompt(valid_input_us)
+        snapshot.assert_match(prompt, "simulation_analysis_prompt_us.txt")
+
+    def test_given_valid_uk_input(self, snapshot):
+
+        snapshot.snapshot_dir = "tests/snapshots"
+
+        prompt = generate_simulation_analysis_prompt(valid_input_uk)
+        snapshot.assert_match(prompt, "simulation_analysis_prompt_uk.txt")
+
+    def test_given_region_is_enhanced_us(self, snapshot):
+
+        snapshot.snapshot_dir = "tests/snapshots"
+        valid_enhanced_us_input_data = (
+            given_valid_data_and_region_is_enhanced_us(valid_input_us)
+        )
+
+        prompt = generate_simulation_analysis_prompt(
+            valid_enhanced_us_input_data
+        )
+        snapshot.assert_match(
+            prompt, "simulation_analysis_prompt_region_enhanced_us.txt"
+        )
+
+    def test_given_dataset_is_enhanced_cps(self, snapshot):
+
+        snapshot.snapshot_dir = "tests/snapshots"
+        valid_enhanced_cps_input_data = (
+            given_valid_data_and_dataset_is_enhanced_cps(valid_input_us)
+        )
+
+        prompt = generate_simulation_analysis_prompt(
+            valid_enhanced_cps_input_data
+        )
+        snapshot.assert_match(
+            prompt, "simulation_analysis_prompt_dataset_enhanced_cps.txt"
+        )
 
     def test_given_missing_input_field(self):
 
