46e126b75affca561c858a13de5364335b81492c
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2025-02-25 15:02:39 +0000

    Update PolicyEngine UK (#2211)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "policyengine_canada==0.96.2",
         "policyengine-ng==0.5.1",
         "policyengine-il==0.1.0",
-        "policyengine_uk==2.19.1",
+        "policyengine_uk==2.19.2",
         "policyengine_us==1.202.2",
         "policyengine_core>=3.11.8",
         "pydantic",
