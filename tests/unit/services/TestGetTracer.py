f20cb2c978cfaedc3da86ed784ade6cd0e194c08
Swetna <48342893+Swetna@users.noreply.github.com>
2025-03-12 16:10:00 -0400

    Update TestGetTracer.py

--- tests/unit/services/TestGetTracer.py
+++ tests/unit/services/TestGetTracer.py
@@ -32,14 +32,21 @@ def test_get_tracer_valid(test_tracer_data):
 
 def test_get_tracer_not_found():
     # Test get_tracer raises NotFound when no matching record exists.
-    data_not_in_db = ["us", "999999", "999", "9.999.0"]
+     valid_country_val_in_db = "us"
+    invalid_household_not_in_db = "9999999"
+    invalid_policyID_not_in_db = "999"
+    invalid_api_version = "9.999.0"
+    data_not_in_db = [valid_country_val_in_db,invalid_household_not_in_db,invalid_policyID_not_in_db,invalid_api_version]
     with pytest.raises(NotFound):
         tracer_service.get_tracer(*data_not_in_db)
 
 
 def test_get_tracer_database_error(test_db):
     # Test get_tracer handles database errors properly.
-    
-    missing_parameter_causing_database_exception = ["", "1000000", "2", "1.150.0"]
+    missing_country_id = ""
+    valid_householdID = "71424"
+    valid_policyID = "2"
+    valid_api_version = "1.150.0"
+    missing_parameter_causing_database_exception = [missing_country_id,valid_householdID , valid_policyID , valid_api_version]
     with pytest.raises(Exception):
         tracer_service.get_tracer(*missing_parameter_causing_database_exception)
