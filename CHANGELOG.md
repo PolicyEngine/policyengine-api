78dbf1cf2bb28e0e861c8eb774ce5d48e9876ffd
SongmingFan <121742950+SongmingFan@users.noreply.github.com>
2025-03-24 15:40:09 -0400

    Updates for create user profile (changelog entry and formatted)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,17 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), 
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+## [3.12.11] - 2025-03-22 13:20:22
+
+### Added
+
+- Create test_create_profile  
+
+## [3.12.10] - 2025-03-10 12:55:10
+
+### Changed
+
+- Update PolicyEngine US to 1.208.0
 
 ## [3.12.9] - 2025-03-05 15:56:31
 
@@ -5367,6 +5378,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 
 
+[3.12.10]: https://github.com/PolicyEngine/policyengine-api/compare/3.12.9...3.12.10
 [3.12.9]: https://github.com/PolicyEngine/policyengine-api/compare/3.12.8...3.12.9
 [3.12.8]: https://github.com/PolicyEngine/policyengine-api/compare/3.12.7...3.12.8
 [3.12.7]: https://github.com/PolicyEngine/policyengine-api/compare/3.12.6...3.12.7
--- tests/unit/services/test_create_profile.py
+++ tests/unit/services/test_create_profile.py
@@ -1,58 +1,70 @@
 import pytest
 import unittest.mock as mock
 from policyengine_api.services.user_service import UserService
+
 user_service = UserService()
+
 
 class TestCreateProfile:
     def test_create_profile_valid(self, test_db):
-        auth0_id = 'test_auth0_id'
-        primary_country = 'us'
-        username = 'test_username'
+        auth0_id = "test_auth0_id"
+        primary_country = "us"
+        username = "test_username"
         user_since = 2025
 
         def fetch_created_record():
             return test_db.query(
-                'SELECT * FROM user_profiles WHERE auth0_id = ?', (auth0_id,)
+                "SELECT * FROM user_profiles WHERE auth0_id = ?", (auth0_id,)
             ).fetchone()
 
         result = user_service.create_profile(
-            primary_country=primary_country, auth0_id=auth0_id, username=username, user_since=user_since
+            primary_country=primary_country,
+            auth0_id=auth0_id,
+            username=username,
+            user_since=user_since,
         )
 
         assert result[0] is True
 
         user_record = fetch_created_record()
         assert user_record is not None
-        assert user_record['auth0_id'] == auth0_id  # Ensure column name is correct
-        assert user_record['primary_country'] == primary_country
-        assert user_record['username'] == username
-        assert user_record['user_since'] == user_since
+        assert (
+            user_record["auth0_id"] == auth0_id
+        )  # Ensure column name is correct
+        assert user_record["primary_country"] == primary_country
+        assert user_record["username"] == username
+        assert user_record["user_since"] == user_since
 
-    
     def test_create_profile_invalid_argument(self, test_db):
-        primary_country = 'us'
-        username = 'test_username'
+        primary_country = "us"
+        username = "test_username"
         user_since = 2025
         with pytest.raises(
             Exception,
             match=r"UserService.create_profile\(\) missing 1 required positional argument: 'auth0_id'",
         ):
             user_service.create_profile(
-                primary_country=primary_country, username=username, user_since=user_since
+                primary_country=primary_country,
+                username=username,
+                user_since=user_since,
             )
-        
 
     def test_create_profile_duplicate(self, test_db):
-        auth0_id = 'test_auth0_id'
-        primary_country = 'us'
-        username = 'test_username'
+        auth0_id = "test_auth0_id"
+        primary_country = "us"
+        username = "test_username"
         user_since = 2025
         result1 = user_service.create_profile(
-            primary_country=primary_country, auth0_id=auth0_id, username=username, user_since=user_since
+            primary_country=primary_country,
+            auth0_id=auth0_id,
+            username=username,
+            user_since=user_since,
         )
-        
-        
+
         result2 = user_service.create_profile(
-                primary_country=primary_country, auth0_id=auth0_id, username=username, user_since=user_since
+            primary_country=primary_country,
+            auth0_id=auth0_id,
+            username=username,
+            user_since=user_since,
         )
         assert result2[0] == False
