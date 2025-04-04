dc583bceeb92338cdbb99222387504843256885b
HerveRI <herve.irad@gmail.com>
2025-04-04 00:24:03 -0400

    Applied Pull Requests Suggestions

--- tests/unit/services/test_execute_analysis.py
+++ tests/unit/services/test_execute_analysis.py
@@ -5,21 +5,28 @@ from policyengine_api.services.tracer_analysis_service import (
 )
 from werkzeug.exceptions import NotFound
 
-from tests.fixtures.services.tracer_analysis_service_fixture import (
+from tests.fixtures.services.tracer_analysis_service import (
     sample_tracer_data,
     sample_expected_segment,
-    get_tracer,
-    get_existing_analysis,
-    parse_tracer_output,
-    trigger_ai_analysis,
+    mock_get_tracer,
+    mock_get_existing_analysis,
+    mock_parse_tracer_output,
+    mock_trigger_ai_analysis,
 )
 
 service = TracerAnalysisService()
+country_id = "us"
+household_id = "71424"
+policy_id = "2"
+target_variable = "takes_up_snap_if_eligible"
 
 
 class TestExecuteAnalysis:
     def test_execute_analysis_static(
-        self, get_tracer, parse_tracer_output, get_existing_analysis
+        self,
+        mock_get_tracer,
+        mock_parse_tracer_output,
+        mock_get_existing_analysis,
     ):
         """
         GIVEN a valid tracer data and an expected parsed segment (included as fixture),
@@ -29,7 +36,7 @@ class TestExecuteAnalysis:
         """
 
         analysis, analysis_type = service.execute_analysis(
-            "us", "71424", "2", "market_income"
+            country_id, household_id, policy_id, target_variable
         )
 
         assert analysis == "Existing static analysis"
@@ -37,25 +44,26 @@ class TestExecuteAnalysis:
 
     def test_execute_analysis_streaming(
         self,
-        get_tracer,
-        parse_tracer_output,
-        get_existing_analysis,
-        trigger_ai_analysis,
+        mock_get_tracer,
+        mock_parse_tracer_output,
+        mock_get_existing_analysis,
+        mock_trigger_ai_analysis,
     ):
         """
         GIVEN a valid tracer data and an expected parsed segment,
         AND get_existing_analysis returns None,
-        WHEN execute_analysis is called ",
+        WHEN execute_analysis is called,
         THEN trigger_ai_analysis  is called and returns a generator with the "streaming" flag.
         """
 
         # When existing analysis value is None
-        get_existing_analysis.return_value = None
+        mock_get_existing_analysis.return_value = None
 
         analysis, analysis_type = service.execute_analysis(
-            "us", "71424", "2", "market_income"
+            country_id, household_id, policy_id, target_variable
         )
 
+        expected_streaming_output = ["stream chunk 1", "stream chunk 2"]
         streaming_output = list(analysis)
-        assert streaming_output == ["stream chunk 1", "stream chunk 2"]
+        assert streaming_output == expected_streaming_output
         assert analysis_type == "streaming"
