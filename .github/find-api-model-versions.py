f8235b90cedb7cb46cab667744fd2d85482b3ce8
Anthony Volk <anth.volk@gmail.com>
2025-06-03 19:17:05 -0400

    fix: Move code to push.yaml, remove from pr.yaml

--- .github/find-api-model-versions.py
+++ .github/find-api-model-versions.py
@@ -2,13 +2,14 @@ import os
 import sys
 from policyengine_api.constants import COUNTRY_PACKAGE_VERSIONS
 
+
 def find_api_model_versions_and_output_to_github():
     """
     Find the API model versions and output them to a file for GitHub.
     """
     # Try to get package versions for US and UK
-    us_version = COUNTRY_PACKAGE_VERSIONS.get('us')
-    uk_version = COUNTRY_PACKAGE_VERSIONS.get('uk')
+    us_version = COUNTRY_PACKAGE_VERSIONS.get("us")
+    uk_version = COUNTRY_PACKAGE_VERSIONS.get("uk")
 
     if not us_version:
         print("Error: US package version not found.", file=sys.stderr)
@@ -18,11 +19,11 @@ def find_api_model_versions_and_output_to_github():
         print("Error: UK package version not found.", file=sys.stderr)
         sys.exit(1)
 
-
     # Write to GitHub Actions environment
-    with open(os.environ['GITHUB_ENV'], 'a') as f:
-        f.write(f'US_VERSION={us_version}\n')
-        f.write(f'UK_VERSION={uk_version}\n')
+    with open(os.environ["GITHUB_ENV"], "a") as f:
+        f.write(f"US_VERSION={us_version}\n")
+        f.write(f"UK_VERSION={uk_version}\n")
+
 
 if __name__ == "__main__":
     find_api_model_versions_and_output_to_github()
