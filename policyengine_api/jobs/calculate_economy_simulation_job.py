2c890fac48d2d89754e7285c95f7ea2ac3363fed
nikhilwoodruff <nikhil.woodruff@outlook.com>
2025-06-06 16:35:51 +0100

    Add error handling

--- policyengine_api/jobs/calculate_economy_simulation_job.py
+++ policyengine_api/jobs/calculate_economy_simulation_job.py
@@ -296,16 +296,22 @@ class CalculateEconomySimulationJob(BaseJob):
                         progress_log.model_dump(mode="json"), severity="INFO"
                     )
                 except Exception as e:
-                    # Send error log to GCP
-                    error_log: V2V1Comparison = V2V1Comparison.model_validate(
-                        {
-                            **comparison_data,
-                            "v2_error": str(e),
-                        }
-                    )
-                    logger.log_struct(
-                        error_log.model_dump(mode="json"), severity="ERROR"
-                    )
+                    try:
+                        # Send error log to GCP
+                        error_log: V2V1Comparison = (
+                            V2V1Comparison.model_validate(
+                                {
+                                    **comparison_data,
+                                    "v2_error": str(e),
+                                }
+                            )
+                        )
+                        logger.log_struct(
+                            error_log.model_dump(mode="json"), severity="ERROR"
+                        )
+                    except Exception as e2:
+                        print("Something really wrong.", e2)
+                        pass
 
             # Compute baseline economy
             logger.log_struct(
