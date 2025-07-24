c485036f6f140bdc524db06df2d0c7816bf31e78
María Juaristi <127882282+juaristi22@users.noreply.github.com>
2025-07-24 14:51:04 +0200

    Update microdf version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "werkzeug",
         "Flask-Caching>=2,<3",
         "google-cloud-logging>=3,<4",
-        "microdf_python==0.4.4",
+        "microdf_python>=1.0.0",
     ],
     extras_require={
         "dev": ["pytest-timeout", "coverage", "pytest-snapshot"],
